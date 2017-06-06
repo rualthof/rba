851c5175b963c9f47b5de5e67264465cc3bc9555
rudimar <rualthof@hotmail.com>
2017-06-06 12:08:35 -0300

    testes gpio

--- epos2/app/led_blink.cc
+++ epos2/app/led_blink.cc
@@ -6,15 +6,51 @@ using namespace EPOS;
 
 OStream cout;
 
+void delay(int max){
+	
+	for(volatile int t=0;t<max;t++);
+}
+
 int main()
 {
+	
+	
+    GPIO uart1rx('C',3, GPIO::OUT);
+    GPIO uart1tx('C',4, GPIO::OUT);
+    GPIO RE('C',5, GPIO::OUT);
+    GPIO DE('C',6, GPIO::OUT);
     
-    GPIO g('C',3, GPIO::OUT);
+    GPIO uart0tx('A',1, GPIO::OUT);
+    GPIO uart0rx('A',0, GPIO::OUT);
+    
+    int max = 0x2ffff;
+    
     for(bool b=false;;b=(b+1)%2)
     {
         cout << "teste\n";
-        g.set(b);
-        for(volatile int t=0;t<0xfffff;t++);
+        uart1rx.set(b); 	delay(max);
+        uart1rx.set(!b);
+        
+        uart1tx.set(b); 	delay(max);
+        uart1tx.set(!b);        
+        
+        RE.set(b); 	delay(max);
+        RE.set(!b);
+        
+        DE.set(b); 	delay(max);
+        DE.set(!b);
+        
+        uart0rx.set(b);		delay(max);
+        uart0rx.set(!b);
+        
+        uart0tx.set(b); 	delay(max);
+        uart0tx.set(!b);        
+        
+        max = max-10000;
+        
+        if(max<=30000)
+			max = 0x2ffff;
+        
     }
     return 0;
 }
